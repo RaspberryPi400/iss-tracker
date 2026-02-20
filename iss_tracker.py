acdcba40b25f214ab7e9c1f2a9b5fd9147505987
Elijah Corwin <raspberrypi500@yahoo.com>
2026-02-20 09:44:56 -0500

    Added logic to show users the people currently on the ISS

--- iss_tracker.py
+++ iss_tracker.py
@@ -1,13 +1,42 @@
 import requests
 import sys
 
+def print_astronauts():
+    # Get the astronauts' names from Open Notify
+    url = "http://api.open-notify.org/astros.json"
+    response = requests.get(url)
+
+    # If successfull:
+    if response.status_code == 200:
+        data = response.json()
+
+        # Get the number of people and their names
+        number = data['number']
+        people = data['people']
+
+        # Print the number of people in space, not just on the ISS
+        print(f"There are currently {number} people in space:")
+        print("----------------------------------------------")
+
+        for person in people:
+            # If the person is on the ISS, print their name(s)
+            if person['craft'] == "ISS":
+                print(f"- {person['name']}")
+            # If no one is currently on the ISS, say that.
+
+    # If it could not get the data, exit the program.
+    else:
+        print("Could not retrieve astronaut data.")
+        print("Exiting...")
+        sys.exit(0)
+
 # The URL for the ISS current location API
 url = "http://api.open-notify.org/iss-now.json"
 
 outer_loop = True
 # Make a outer loop so the user can run the code again.
 while outer_loop:
-# Make a inner loop so the user can run the code again.
+# Make a inner loop so the user can run the code again
     running = True
     while running:
         # Make the request to get the current location of the ISS
@@ -21,11 +50,14 @@ while outer_loop:
             # Convert the latitude and longitude to floats, as they are returned as strings.
             lat = float(data['iss_position']['latitude'])
             lon = float(data['iss_position']['longitude'])
+
+            # Print the astronauts currently on the ISS
+            print_astronauts()
     
             # Print the current location
             print(f"The ISS's Current Location --> Latitude: {lat}; Longitude: {lon}")
-            again = input("Would you like to see the location again? ('y'/'n'): ")
-            # Get the location again and print it if the user wants to see it again
+            again = input("Would you like to see the data again? ('y'/'n'): ")
+            # Get the data again and print it if the user wants to see it again (the location is always changing)
             if again == "y":
                 break
             # Exit the program if the user is done
